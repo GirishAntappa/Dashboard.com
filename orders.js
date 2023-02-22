99859a1c6c8ec9afc979d13e0549d9208179c76d
GirishAntappa <123067510+GirishAntappa@users.noreply.github.com>
2023-02-22 15:50:33 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber:'36378',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber:'49525',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber:'55663',
+        paymentStatus:'paid',
+        shipping:'Deleverd'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber:'55266',
+        paymentStatus:'Paid',
+        shipping:'Delevered'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber:'63542',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName: 'lozenge Drone',
+        productNumber:'00254',
+        paymentStatus:'paid',
+        shipping:'Deleverd'
+    },
+]
